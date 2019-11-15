ef8ebb651ac3804ccb16cc383f00846d9ee3392c
Muran94 <headiv94@gmail.com>
2019-11-15 12:44:03 +0900

    ファイル名やクラス名を修正

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,11 @@
-class JudgeTriangle
+class Triangle
   def initialize(x1, y1, x2, y2, x3, y3)
     @coordinate1 = x1, y1
     @coordinate2 = x2, y2
     @coordinate3 = x3, y3
   end
   
-  def triangle?
+  def is_triangle?
     return false if on_the_same_line?(@coordinate1, @coordinate2, @coordinate3) # 3点の座標の、x座標が同一直線上にあるか
     if on_the_same_line?(@coordinate1, @coordinate2, @coordinate3) # 3点の座標の、x座標が同一直線上にあるか
       false
