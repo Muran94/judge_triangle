7164829fd99f923616dc67f04aa7d1c99e8d54e0
Muran94 <headiv94@gmail.com>
2019-11-15 12:40:39 +0900

    main.rb修正2

--- main.rb
+++ main.rb
@@ -1,13 +1,18 @@
 require_relative 'lib/judge_triangle'
 
+puts "-" * 75
 puts %(与えられた座標が整数値の時)
+puts "-" * 75
 puts %(A(1, 1), B(2, 3), C(2, -1)  => #{JudgeTriangle.new(1, 1, 2, 3, 2, -1).triangle?})
 puts %(A(1, 1), B(1, 3), C(1, -1)  => #{JudgeTriangle.new(1, 1, 1, 3, 1, -1).triangle?})
 puts %(A(1, 1), B(3, 1), C(-2, 1)  => #{JudgeTriangle.new(1, 1, 3, 1, -2, 1).triangle?})
 puts %(A(-3, -3), B(0, 0), C(3, 3) => #{JudgeTriangle.new(-3, -3, 0, 0, 3, 3).triangle?})
 puts %(A(0, 0), B(0, 0), C(0, 0)   => #{JudgeTriangle.new(0, 0, 0, 0, 0, 0).triangle?})
 
+puts "\n\n"
+puts "-" * 75
 puts %(与えられた座標が不動小数値の時（小数点２桁以下四捨五入）)
+puts "-" * 75
 puts %(A(1.111, 1.111), B(2.222, 3.333), C(2.222, -1.111)  => #{JudgeTriangle.new(1.111, 1.111, 2.222, 3.333, 2.222, -1.111).triangle?})
 puts %(A(1.111, 1.111), B(1.111, 3.333), C(1.111, -1.111)  => #{JudgeTriangle.new(1.111, 1.111, 1.111, 3.333, 1.111, -1.111).triangle?})
 puts %(A(1.111, 1.111), B(3.333, 1.111), C(-2.222, 1.111)  => #{JudgeTriangle.new(1.111, 1.111, 3.333, 1.111, -2.222, 1.111).triangle?})
